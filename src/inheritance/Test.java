c5fd3b3c1fa130d8f50c296b62f5fdd88bde5048
Ram Gade <ramdas.gade23@gmail.com>
2024-12-07 16:09:01 +0530

    local changes

--- src/inheritance/Test.java
+++ src/inheritance/Test.java
@@ -2,11 +2,10 @@ package inheritance;
 
 public class Test {
     public static void main(String[] args) {
-
-        /*B b = new B();
+        B b = new B();
         System.out.println(b.y);
         System.out.println(b.x);
-        b.msg();*/
+        b.msg();
 
         Car car = new Car();
         System.out.println(car.brand + " " + car.modelName);
