4191ad676b683449762331b6915aad6bfad0653a
Ram Gade <ramdas.gade23@gmail.com>
2025-01-02 19:30:39 +0530

    Lecture jan-02-2025

---
+++ src/multithreading/MyThread.java
@@ -0,0 +1,37 @@
+package multithreading;
+
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) { //Stage 3: running
+            try {
+                Thread.sleep(2000); //stage 4 : waiting for 2 seconds <-> running
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + " " + i); //Stage 3 : running
+        }
+    }
+
+    public MyThread(String name) {
+        super(name);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
