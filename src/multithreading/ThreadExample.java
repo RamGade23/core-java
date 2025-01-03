24e67de227ed99d5b4d33308c018cc3e6617b3f0
Ram Gade <ramdas.gade23@gmail.com>
2025-01-03 22:16:48 +0530

    Lecture jan-03-2025

--- src/multithreading/ThreadExample.java
+++ src/multithreading/ThreadExample.java
@@ -3,12 +3,22 @@ package multithreading;
 public class ThreadExample {
     public static void main(String[] args) throws InterruptedException {
 
-        MyThread t1 = new MyThread("abc"); //stage 1 : New
+        MyThread t1 = new MyThread("abc-1"); //stage 1 : New
+        MyThread t2 = new MyThread("abc-2"); //stage 1 : New
         t1.start(); //stage 2 : Runnable
+        t2.start(); //stage 2 : Runnable
         //Thread.sleep(2000);
-        System.out.println("inside main method");
+        //System.out.println("inside main method");
         //t1.setName("XYZ");
         //System.out.println("t1.getName() : " + t1.getName());
+
+        /*ThreadWithRunnable threadWithRunnable = new ThreadWithRunnable();
+        Thread thread = new Thread(threadWithRunnable);
+        Thread thread1 = new Thread(threadWithRunnable);
+        thread.setName("t-1");
+        thread1.setName("t-2");
+        thread.start();
+        thread1.start();*/
     }
 }
 
